07960afe4d83e119442c828e9045e82a091d790c
aubrydreikosen <adreiko@live.unc.edu>
2021-11-29 20:54:42 -0500

    split into 2 databases

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
-// Define app using express
 var express = require("express")
 var app = express()
-// Require database SCRIPT file
 var db = require('./database.js')
-// Require md5 MODULE
 var md5 = require("md5")
-//require cors module
 var cors = require("cors");
-// Make Express use its own built-in body parser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-//make express use cors
 app.use(cors());
 
-// Set server port
+
 var HTTP_PORT = 5000;
-// Start server
+
 app.listen(HTTP_PORT, () => {
     console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
 });
@@ -39,17 +33,21 @@ app.post("/app/new/user", (req, res) => {
 	res.status(201).json({"message":info.changes +" record created: ID " + info.lastInsertRowid + " (201)"});
 });
 
+
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
 app.get("/app/users/", (req, res) => {	
 	const stmt = db.prepare("SELECT * FROM userinfo").all();
 	res.status(200).json(stmt);
 });
 
+
+/*
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
 app.get("/app/user/:user", (req, res) => {	
 	const stmt = db.prepare("SELECT * FROM userinfo WHERE user = ?").get(req.body.user);
 	res.status(200).json(stmt);
 });
+*/
 
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 //might also need to add an update score portion
@@ -71,31 +69,8 @@ app.delete("/app/delete/user/:user", (req, res) => {
 	res.status(200).json({"message":stmt.changes +" record deleted: ID " + req.body.user + " (200)"});
 });
 
-// Log in (HTTP method GET) at endpoint /app/user/login
-app.get("/app/user/login", (req, res) => {	
-	var data = {
-		user: req.body.user,
-		pass: req.body.pass ? md5(req.body.pass): null,
-	}
-	const stmt = db.prepare("SELECT * FROM userinfo WHERE user = ? AND pass = ?")
-	const info = stmt.get(data.user, data.pass);
-	res.status(200).json(info);
-});
-
-
 // Default response for any other request
 app.use(function(req, res){
 	res.json({"message":"Endpoint not found. (404)"});
     res.status(404);
-});
-
-//log in 
-app.post("/app/new/user", (req, res) => {	
-	var data = {
-		user: req.body.user,
-		pass: req.body.pass ? md5(req.body.pass): null,
-	}
-	const stmt = db.prepare("INSERT INTO userinfo (user, pass, email) VALUES (?, ?, ?)")
-	const info = stmt.run(data.user, data.pass, data.email);
-	res.status(201).json({"message":info.changes +" record created: ID " + info.lastInsertRowid + " (201)"});
 });
