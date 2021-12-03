512a0a2c5b3ca76ec7a0b2fe05a52187abac1d49
aubrydreikosen <adreiko@live.unc.edu>
2021-12-03 15:54:43 -0500

    working endpoints!

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var app = express()
 //user database
 var db = require('./database.js')
 //interaction database
-var dbi = require('./interactionDatabase.js')
 var md5 = require("md5")
 var cors = require("cors");
 app.use(express.urlencoded({ extended: true }));
@@ -12,7 +11,6 @@ app.use(express.json());
 app.use(cors());
 
 var numRows = 0;
-
 
 var HTTP_PORT = 5000;
 
@@ -80,43 +78,21 @@ app.patch("/app/update/user/logged/:logged", (req, res) => {
 	res.status(200).json({"message":info.changes +" record updated: ID " + req.body.user + " (200)"});
 });
 
+// UPDATE a single user to be logged in (HTTP method PATCH) at endpoint /app/update/user/:user/password/:password
+//might also need to add an update score portion
+app.patch("/app/update/user/logoff/:logged", (req, res) => {	
+	const stmt = db.prepare("UPDATE userinfo SET logged = COALESCE(0, logged) WHERE logged=?");
+	const info = stmt.run(req.params.logged);
+	res.status(200).json({"message":info.changes +" record updated: ID " + req.body.user + " (200)"});
+});
+
 // DELETE a single user (logged in user) (HTTP method DELETE) at endpoint /app/delete/logged/:logged
 //return changes in message
 app.delete("/app/delete/logged/:logged", (req, res) => {	
 	const stmt = db.prepare("DELETE FROM userinfo WHERE logged = ?").run(req.params.logged);
 	res.status(200).json({"message":stmt.changes +" record deleted: ID " + req.body.user + " (200)"});
 });
-//Define interaction endpoints
- 
-//----------------------------------------------------------------------------------------------
-// CREATE a new user (HTTP method POST) at endpoint /app/new/interaction
-app.post("/app/new/interaction", (req, res) => {	
-	var data = {
-		user: req.body.user,
-		pass: req.body.pass ? md5(req.body.pass): null,
-	}
-	const stmt = dbi.prepare("INSERT INTO currentuser (user, pass) VALUES (?, ?)")
-	const info = stmt.run(data.user, data.pass);
-	res.status(201).json({"message":info.changes +" record created: ID " + info.lastInsertRowid + " (201)"});
-});
-// READ a list of all users (HTTP method GET) at endpoint /app/interactions
-app.get("/app/interactions/", (req, res) => {	
-	const stmt = dbi.prepare("SELECT * FROM currentuser").all();
-	res.status(200).json(stmt);
-});
 
-// READ a single user (HTTP method GET) at endpoint /app/user/:id
-app.get("/app/interaction/:id", (req, res) => {	
-	const stmt = dbi.prepare("SELECT * FROM currentuser WHERE id = ?").get(req.params.id);
-	res.status(200).json(stmt);
-});
-
-// DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:email
-//return changes in message
-app.delete("/app/delete/interaction/:id", (req, res) => {	
-	const stmt = dbi.prepare("DELETE FROM currentuser WHERE id = ?").run(req.params.id);
-	res.status(200).json({"message":stmt.changes +" record deleted: ID " + req.body.user + " (200)"});
-});
 // Default response for any other request
 app.use(function(req, res){
 	res.json({"message":"Endpoint not found. (404)"});
